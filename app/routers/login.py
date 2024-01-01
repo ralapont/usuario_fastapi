322cff110be66ed029b541ba5062d23b65292455
DESKTOP-ALN97IQ\rafae <rafael.alapont.gomez@gmail.com>
2024-01-01 15:02:38 +0100

    token jwt

--- app/routers/login.py
+++ app/routers/login.py
@@ -1,5 +1,9 @@
+from datetime import datetime, timedelta
+from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from app.db.models import User_model
 import app.repository.crud as crud
 from app.db.database import get_db
 from sqlalchemy.orm import Session
@@ -7,8 +11,11 @@ from passlib.context import CryptContext
 
 
 router = APIRouter(prefix="/token", tags=["token"])
+oauth2_scheme = OAuth2PasswordBearer("/token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRECT_KEY = "c6f59d10a4b67552fcd156fa86fd911e07dc28f5ce4808dd673e937c6919023c"  # openssl rand -hex 32
+ALGORITHM = "HS256"
 
 def get_user(db, username):
     usuario = crud.get_user_by_username(db, username)
@@ -31,18 +38,49 @@ def authenticate_user(db, username, password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credencials",
                             headers={"WWW-Authenticate": "Bearer"})
     return user        
+    
+def create_token(data: dict, time_expired: Optional[datetime] = None):
+    data_copy = data.copy()
+    if not time_expired:
+        expires = datetime.utcnow() + timedelta(minutes=15)
+    else:
+        expires = datetime.utcnow() + time_expired
+    data_copy.update({"exp": expires})
+    token_jwt = jwt.encode(data_copy, key=SECRECT_KEY , algorithm=ALGORITHM)
+    print("Token jwt: {}".format(token_jwt))
+    return token_jwt
 
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        token_decode = jwt.decode(token=token, key=SECRECT_KEY, algorithms=[ALGORITHM])
+        username = token_decode.get("sub")
+        if not username:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credencials",
+                                headers={"WWW-Authenticate": "Bearer"})
+    except JWTError:  
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credencials",
+                                headers={"WWW-Authenticate": "Bearer"})
+    current_user = get_user(db, username=username)
+    print("Current user: {}".format(current_user.__repr__()))
+    return current_user
+
+def get_user_disable_current(user: User_model = Depends(get_current_user)):
+    if not user.estado:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive User")
+    return user
     
         
-    pass
-    
 @router.post("/")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     print("Userdata: {} password: {}".format(form_data.username, form_data.password))
     user = authenticate_user(db, form_data.username, form_data.password)
+    access_token_expires = timedelta(minutes=30)
+    access_token_jwt = create_token({"sub": user.username}, access_token_expires)
+    print("Access token expires: {}".format(access_token_expires))
+    
     if user:
         print("User authenticated : {}".format(user.__repr__()))
     return {
-        "access_token": "tomatito",
+        "access_token": access_token_jwt,
         "token_type": "bearer"
     }
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter, HTTPException, status, Depends
+from app.db.models import User_model
+from app.routers.login import get_user_disable_current
 from app.schemas import User, UserResponse, UserResumido, UserUpdate
 from app.db.database import get_db
 from sqlalchemy.orm import Session
 import app.repository.crud as crud
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/user", tags=["users"])
-oauth2_scheme = OAuth2PasswordBearer("/token")
+#oauth2_scheme = OAuth2PasswordBearer("/token")
 
 @router.get("/", response_model=list[UserResponse])
-async def get(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def get(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), current_user: User_model = Depends(get_user_disable_current)):
+    print("Current user: {}".format(current_user))
     data = crud.get_users(db, skip=skip, limit=limit)
     print("Data: {}".format(data))
     if data:
@@ -17,21 +19,23 @@ async def get(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail="User not exists")
  
 @router.get("/{user_id}", response_model=UserResumido)
-async def get_user(user_id:int, db: Session = Depends(get_db), token:str = Depends(oauth2_scheme),):
-    print("token: {}".format(token))
+async def get_user(user_id:int, db: Session = Depends(get_db), current_user: User_model = Depends(get_user_disable_current)):
+    print("Current user: {}".format(current_user))
     data = crud.get_user(db, user_id)
     if data:
         return data
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail="User not exists")
 
 @router.post('/', response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-async def create_user(user_new:User, db: Session = Depends(get_db)):
+async def create_user(user_new:User, db: Session = Depends(get_db), current_user: User_model = Depends(get_user_disable_current)):
+    print("Current user: {}".format(current_user))
     print("El usuario es: {}".format(user_new))
     data = crud.create_user(db, user_new)
     return data
 
 @router.delete("/{user_id}", response_model=int, status_code=status.HTTP_205_RESET_CONTENT)
-async def remove_user(user_id:int, db: Session = Depends(get_db)):
+async def remove_user(user_id:int, db: Session = Depends(get_db), current_user: User_model = Depends(get_user_disable_current)):
+    print("Current user: {}".format(current_user))
     user_delete_id = crud.delete_user(db, user_id)
     if user_delete_id:
         print("El usuario delete es: {}".format(user_delete_id))
@@ -39,7 +43,8 @@ async def remove_user(user_id:int, db: Session = Depends(get_db)):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail="User not exists")
 
 @router.patch('/{user_id}', response_model=UserResponse, status_code=status.HTTP_200_OK)
-async def update_user(user_id:int, user_new: UserUpdate, db: Session = Depends(get_db)):
+async def update_user(user_id:int, user_new: UserUpdate, db: Session = Depends(get_db), current_user: User_model = Depends(get_user_disable_current)):
+    print("Current user: {}".format(current_user))
     print("El new user is: {} id: {}".format(user_new, user_id))
     
     user_update = crud.update_user(db, user_id, user_new)
